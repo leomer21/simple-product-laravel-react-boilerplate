2f952df715c009818cc20006b7c7c4e20288c4cc
leomer21 <leomerromero2111@gmail.com>
2024-10-12 09:04:55 +0900

    Change folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography } from "antd";
 
-import ProductListing from "./ProductListing";
+import ProductListing from "./pages/ProductListing";
 
 const { Title } = Typography;
 
